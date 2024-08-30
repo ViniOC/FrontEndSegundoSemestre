3ff9d8adc7763539b933a30b4864f1d852ed82bb
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-30 11:52:09 -0300

    Aula de route dom

---
+++ react-router-dom/src/global-styled.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+
+    #root{
+        width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
+    main{
+        flex-grow: 1;
+    }
+
+
+
+
+
+`
+
+export default GlobalStyle
