951f2f1cf1b08cfebc597d301a26883bd9abee59
Vinicius Coutinho <viniciusoliveiracoutinho@gmail.com>
2024-10-08 11:49:03 -0300

    Tailwind+crud- Aula 08-10

---
+++ aula-10-08/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Cabecalho from "@/components/Cabecalho";
+import Rodape from "@/components/Rodape";
+
+
+export default function Home() {
+  return (
+    <main className="grow">
+      
+      <h1 className="text-center text-3xl text-indigo-600 font-bold">Home</h1>
+    
+    </main>
+  );
+}
---
+++ aula-10-08/src/app/produtos/produto/[id]/page.tsx
@@ -0,0 +1,12 @@
+import { TipoProduto } from "@/types"
+import { useState } from "react"
+
+export default function Produto({params}:{params:{id:number}}){
+    
+    cont [produto, setProduto] = useState<TipoProduto>()
+
+    
+    return(
+        <main></main>
+    )
+}
